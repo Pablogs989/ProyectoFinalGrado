58fffe69a23213edc2b96e5b8d553f3565a70280
xiqufe <xiqufe@floridauniversitaria.es>
2022-02-14 18:17:56 +0100

    English

--- src/screens/DocInput_Screen.js
+++ src/screens/DocInput_Screen.js
@@ -1,19 +1,15 @@
 import React from 'react';
-import {Dimensions, StyleSheet, View} from 'react-native';
-import {Appbar, Caption ,Button, Headline, Paragraph, Provider, Subheading, Text, Title} from 'react-native-paper';
+import { Dimensions, StyleSheet, View } from 'react-native';
+import { Appbar, Caption, Button, Headline, Paragraph, Provider, Subheading, Text, Title } from 'react-native-paper';
 import Appbar_Common from '../components/Appbar_Common';
 
 
-const DocInput_Screen = ({navigation}) => {
+const DocInput_Screen = ({ navigation }) => {
     return (
         <Provider>
             <Appbar_Common alPresionar={() => navigation.navigate("Main_Screen")} titulo="Scanner" />
             <View style={styles.box}>
                 <Surface style={styles.falseCard}>
-                    
-
-
-                    
                 </Surface>
             </View>
         </Provider>
@@ -23,19 +19,19 @@ const DocInput_Screen = ({navigation}) => {
 export default DocInput_Screen;
 
 const styles = StyleSheet.create({
-    box:{
-        flex:1,
+    box: {
+        flex: 1,
         backgroundColor: '#26528C',
         height: Dimensions.get("screen").height,
         alignItems: "center"
     },
 
-    falseCard:{
-        backgroundColor:'#A7CAD9',
-        borderRadius:20,
-        height: Dimensions.get("screen").height*81/100,
-        width: Dimensions.get("screen").width*90/100,
-        marginTop: Dimensions.get("screen").height*2/100,
+    falseCard: {
+        backgroundColor: '#A7CAD9',
+        borderRadius: 20,
+        height: Dimensions.get("screen").height * 81 / 100,
+        width: Dimensions.get("screen").width * 90 / 100,
+        marginTop: Dimensions.get("screen").height * 2 / 100,
         padding: 10
     }
 })
